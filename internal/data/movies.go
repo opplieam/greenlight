b90eaa7f1798203a1a57628d2b37754ffbb3f1c6
Opp Lieam <runicer156@gmail.com>
2024-05-01 04:31:57 +0700

    custom runtime type implementing own MarshalJSON()

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genre     []string  `json:"genre,omitempty"`
 	Version   int32     `json:"version"`
 }
