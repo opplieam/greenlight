4c12a59b9391b39f1675de8b2b5fd8ee5d5f5fdc
Opp Lieam <runicer156@gmail.com>
2024-05-02 03:54:29 +0700

    add update movie to database logic

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -79,3 +79,19 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 func (m MovieModel) Delete(id int64) error {
 	return nil
 }
+
+func (m MovieModel) Update(movie *Movie) error {
+	query := `
+		UPDATE movies
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version`
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+}
