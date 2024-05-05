e7fd2cc1da1cf250e2e37af46058a81da29b5007
Opp Lieam <runicer156@gmail.com>
2024-05-06 02:01:05 +0700

    add goroutine background helper function

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -48,12 +48,12 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	go func() {
+	app.background(func() {
 		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
-	}()
+	})
 
 	if err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil); err != nil {
 		app.serverErrorResponse(w, r, err)
