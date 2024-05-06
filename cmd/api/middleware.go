adab3639988e8ac64c1349133f1955dd5b122b7a
Opp Lieam <runicer156@gmail.com>
2024-05-07 04:20:36 +0700

    add authentication middleware and use it in /v1/movies*

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -69,6 +69,24 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if user.IsAnonymous() {
+			app.authenticationRequiredResponse(w, r)
+			return
+		}
+
+		if !user.Activated {
+			app.inactiveAccountResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (app *application) rateLimit(next http.Handler) http.Handler {
 	// IP rate limiter
 
